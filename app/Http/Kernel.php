b0c1dd4b9031e946f75afcfc9a5546986a891d66
Michael Loyselle <52831149+Leozas@users.noreply.github.com>
2019-12-04 13:44:56 -0500

    gcloud init

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,10 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Spatie\Cors\Cors::class
+
     ];
+
 
     /**
      * The application's route middleware groups.
